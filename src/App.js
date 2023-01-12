57d7d32810e18184c1e6e0416cb30e5fa2c92659
Nate Trench <nate.trench@appcues.com>
2023-01-12 16:57:51 -0600

    push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter} from "react-router-dom";
-import { logEvents } from "./appcuesComponents/EventLog";
 import AppcuesRouter from "./Appcues";
 
 /*
@@ -23,26 +22,7 @@ const App = () => {
   if (!window.localStorage.timestamp) { // persists the timestamp across refresh
     window.localStorage.setItem("timestamp", Date.now()); // if no timestamp, sets it to now
   }
-  
-  const userId = window.localStorage.currentUser;
-  const currentTime = parseInt(window.localStorage.timestamp);
 
- // window.Appcues.identify(userId, { // feel free to add in any user properties here
- //   role: "tester",
- //   signupDate: currentTime
- // })
-
- // window.Appcues.on("all", function(name, payload) {
-    // console.log(name);     // uncomment these two lines if you'd like to
-    // console.log(payload);  // log all events into the dev tools console
-
-  //  logEvents(name, payload); // adds events into the event log on the page
- // })
-
- // window.Appcues.loadLaunchpad("#launchpad", { // loads Launchpad on this element
-   // position: "right"
-  //})
-  
   return ( 
     <BrowserRouter> 
       <AppcuesRouter /> 
